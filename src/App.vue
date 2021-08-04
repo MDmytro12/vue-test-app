6aeb7a2b76675162dd1fa5f21b35f5baf2285dbe
Dimasta <dimas1969592@gmail.com>
2021-08-04 22:06:51 +0300

    Last 30 minutes of course!

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <template>
   <div id="app">
-   Hhhhh
+   <h1> Todos apllication  </h1>
+   <hr>
+    <router-view/>
+
+    <NavBar />
   </div>
 </template>
 
 <script>
-
+import TodoList from '@/components/Todo'
+import NavBar from '@/components/NavBar'
 
 export default {
   name: 'App',
+  methods: {
+    removeTodo( idd ){
+      this.todos = this.todos.filter(it => it.id != idd)
+    },
+    addTodo(newTodo){
+      this.todos.push(newTodo)
+    }
+  },
   components: {
-  
+    TodoList ,
+    NavBar
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+    h1{
+      font-size: 120px;
+      color: green;
+      border: 2px crashed grey;
+      text-align: center;
+    }
 </style>
